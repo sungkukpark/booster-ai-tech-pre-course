644b1d27cd24199d7cde4ee74096e3d43fb40df5
Sungkuk Park <sungkukshawnpark@gmail.com>
2025-08-05 18:23:48 +0200

    fix: update execution counts in Jupyter notebook cells

--- example.py
+++ example.py
@@ -1,12 +1,14 @@
 def f(x):
     return 2 * x + 7
 
-
 def g(x):
-    return x**2
-
+    return x * 2
 
 if __name__ == "__main__":
     x = 2
+    print(f(x))
+    print(g(x))
+    print(f(g(x)))
+    print(g(f(x)))
     result = f(x) + g(x) + f(g(x)) + g(f(x))
     print(result)
